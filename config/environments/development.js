1cabf0e8362a9a5d002efc4f6178d98f4daf2cc4
Simon Tokumine <simon@japancentre.com>
2011-09-06 17:38:36 +0100

    update port

--- config/environments/development.js
+++ config/environments/development.js
@@ -4,4 +4,4 @@ module.exports.redis            = {host: '127.0.0.1',
                                    port: 6379, 
                                    idleTimeoutMillis: 1,
                                    reapIntervalMillis: 1};
-module.exports.windshaft_port   = 8080;
+module.exports.windshaft_port   = 8181;
