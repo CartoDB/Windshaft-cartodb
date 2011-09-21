2e1b36a1a45b7c5388f5fd4b57bac75ae0217414
Simon Tokumine <simon@japancentre.com>
2011-09-21 16:33:25 -0400

    configure tileuser and mapkey security

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,4 +1,4 @@
 module.exports.name             = 'production';
-module.exports.postgres         = {user: 'publicuser', host: '127.0.0.1', port: 6432};
+module.exports.postgres         = {user: 'tileuser', host: '127.0.0.1', port: 6432};
 module.exports.redis            = {host: '127.0.0.1', port: 6379};
 module.exports.windshaft_port   = 8181;
