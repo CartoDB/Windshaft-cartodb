1bcf1f977810168cc15b002af6e99c5eae9763ee
Simon Tokumine <simon@japancentre.com>
2011-11-29 20:50:32 +0100

    enable cache

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,7 +1,7 @@
 module.exports.name             = 'production';
-module.exports.postgres         = {user: 'tileuser', host: '127.0.0.1', port: 6432, max_size: 6};
+module.exports.postgres         = {user: 'tileuser', host: '127.0.0.1', port: 6432, max_size: 10};
 module.exports.redis            = {host: '127.0.0.1', port: 6379};
 module.exports.windshaft_port   = 8181;
-module.exports.lru_cache = false;
+module.exports.lru_cache = true;
 module.exports.lru_cache_size = 10000;
 module.exports.ttl_timeout = 600; // 10 minutes
