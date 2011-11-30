c92d16fd5ec1cfb0c2c85627b314bf866b510a78
Simon Tokumine <simon@japancentre.com>
2011-11-30 17:49:08 +0100

    bump windshaft

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,5 +1,5 @@
 module.exports.name             = 'production';
-module.exports.postgres         = {user: 'tileuser', host: '127.0.0.1', port: 6432, max_size: 10};
+module.exports.postgres         = {user: 'tileuser', host: '127.0.0.1', port: 6432};
 module.exports.redis            = {host: '127.0.0.1', port: 6379};
 module.exports.windshaft_port   = 8181;
 module.exports.lru_cache = true;
