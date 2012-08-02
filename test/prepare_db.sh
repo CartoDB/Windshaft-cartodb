651c45bc1687a5b62331c8fc3e4a5706655d6751
Sandro Santilli <strk@keybit.net>
2012-08-02 10:42:12 +0200

    Use the correct redis variable for checking map key (closes #39)
    Includes testcase

--- test/prepare_db.sh
+++ test/prepare_db.sh
@@ -28,7 +28,8 @@ psql "${TEST_DB}" < ./sql/gadm4.sql
 echo "preparing redis..."
 echo "HSET rails:users:vizzuality id 1" | redis-cli -p ${REDIS_PORT} -n 5
 echo 'HSET rails:users:vizzuality database_name "'"${TEST_DB}"'"' | redis-cli -p ${REDIS_PORT} -n 5
-echo "SADD rails:users:vizzuality:map_key 1234" | redis-cli -p ${REDIS_PORT} -n 5
+echo "HSET rails:users:vizzuality map_key 1234" | redis-cli -p ${REDIS_PORT} -n 5
+echo "SADD rails:users:vizzuality:map_key 1235" | redis-cli -p ${REDIS_PORT} -n 5
 echo 'HSET rails:'"${TEST_DB}"':my_table infowindow "this, that, the other"' | redis-cli -p ${REDIS_PORT} -n 0
 
 echo "Finished preparing data. Run tests with expresso."
