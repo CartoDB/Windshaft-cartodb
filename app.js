0b3fdb07f64524821ed78618a09b89269dee3037
Sandro Santilli <strk@keybit.net>
2013-11-18 10:07:28 +0100

    Drop unneeded include from outermost app

--- app.js
+++ app.js
@@ -25,10 +25,6 @@ global.settings     = require(__dirname + '/config/settings');
 global.environment  = require(__dirname + '/config/environments/' + ENV);
 _.extend(global.settings, global.environment);
 
-// Include cart_data.js only _after_ the "global" variable is set
-// See https://github.com/Vizzuality/Windshaft-cartodb/issues/28
-var cartoData = require('./lib/cartodb/carto_data');
-
 var Windshaft = require('windshaft');
 var serverOptions = require('./lib/cartodb/server_options');
 
