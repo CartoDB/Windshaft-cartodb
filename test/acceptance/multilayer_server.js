434d6d4110720b4217067f2f5b71d8d51c6c2034
Raul Ochoa <rochoaf@gmail.com>
2015-03-30 13:42:59 +0200

    Extract request function

--- test/acceptance/multilayer_server.js
+++ test/acceptance/multilayer_server.js
@@ -46,15 +46,22 @@ describe('tests from old api translated to multilayer', function() {
         };
     }
 
+    function createRequest(layergroup, userHost) {
+        return {
+            url: layergroupUrl,
+            method: 'POST',
+            headers: {
+                host: userHost || 'localhost',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(layergroup)
+        };
+    }
+
     it("layergroup creation fails if CartoCSS is bogus", function(done) {
         var layergroup = singleLayergroupConfig(wadusSql, '#my_table3{');
         assert.response(server,
-            {
-                url: layergroupUrl,
-                method: 'POST',
-                headers: {host: 'localhost', 'Content-Type': 'application/json' },
-                data: JSON.stringify(layergroup)
-            },
+            createRequest(layergroup),
             {
                 status: 400
             },
@@ -70,12 +77,7 @@ describe('tests from old api translated to multilayer', function() {
     it("multiple bad styles returns 400 with all errors", function(done) {
         var layergroup = singleLayergroupConfig(wadusSql, '#my_table4{backgxxxxxround-color:#fff;foo:bar}');
         assert.response(server,
-            {
-                url: layergroupUrl,
-                method: 'POST',
-                headers: {host: 'localhost', 'Content-Type': 'application/json' },
-                data: JSON.stringify(layergroup)
-            },
+            createRequest(layergroup),
             {
                 status: 400
             },
@@ -99,12 +101,7 @@ describe('tests from old api translated to multilayer', function() {
         var layergroup = singleLayergroupConfig(sql, cartocss);
 
         assert.response(server,
-            {
-                url: layergroupUrl,
-                method: 'POST',
-                headers: {host: 'localhost', 'Content-Type': 'application/json' },
-                data: JSON.stringify(layergroup)
-            },
+            createRequest(layergroup),
             {
                 status: 200
             },
