70d1a30c64079dbf881c2d3e83f00542dcb46c02
Raul Ochoa <rochoaf@gmail.com>
2015-03-30 13:30:57 +0200

    style

--- test/acceptance/multilayer_server.js
+++ test/acceptance/multilayer_server.js
@@ -1,15 +1,18 @@
 require(__dirname + '/../support/test_helper');
 
-var assert      = require('../support/assert');
+var assert = require('../support/assert');
 
-var redis       = require('redis');
+var redis = require('redis');
+var _ = require('underscore');
 
-var CartodbWindshaft = require(__dirname + '/../../lib/cartodb/cartodb_windshaft');
-var serverOptions = require(__dirname + '/../../lib/cartodb/server_options')();
+var CartodbWindshaft = require('../../lib/cartodb/cartodb_windshaft');
+var serverOptions = require('../../lib/cartodb/server_options')();
 var server = new CartodbWindshaft(serverOptions);
 server.setMaxListeners(0);
 
 describe('tests from old api translated to multilayer', function() {
+
+    var test_database = _.template(global.environment.postgres_auth_user, {user_id:1}) + '_db';
 
     var layergroupUrl = '/api/v1/map';
 
@@ -45,36 +48,46 @@ describe('tests from old api translated to multilayer', function() {
 
     it("layergroup creation fails if CartoCSS is bogus", function(done) {
         var layergroup = singleLayergroupConfig(wadusSql, '#my_table3{');
-        assert.response(server, {
-            url: layergroupUrl,
-            method: 'POST',
-            headers: {host: 'localhost', 'Content-Type': 'application/json' },
-            data: JSON.stringify(layergroup)
-        }, {}, function(res) {
-            assert.equal(res.statusCode, 400, res.body);
-            var parsed = JSON.parse(res.body);
-            assert.ok(parsed.errors[0].match(/^style0/));
-            assert.ok(parsed.errors[0].match(/missing closing/));
-            done();
-        });
+        assert.response(server,
+            {
+                url: layergroupUrl,
+                method: 'POST',
+                headers: {host: 'localhost', 'Content-Type': 'application/json' },
+                data: JSON.stringify(layergroup)
+            },
+            {
+                status: 400
+            },
+            function(res) {
+                var parsed = JSON.parse(res.body);
+                assert.ok(parsed.errors[0].match(/^style0/));
+                assert.ok(parsed.errors[0].match(/missing closing/));
+                done();
+            }
+        );
     });
 
     it("multiple bad styles returns 400 with all errors", function(done) {
         var layergroup = singleLayergroupConfig(wadusSql, '#my_table4{backgxxxxxround-color:#fff;foo:bar}');
-        assert.response(server, {
-            url: layergroupUrl,
-            method: 'POST',
-            headers: {host: 'localhost', 'Content-Type': 'application/json' },
-            data: JSON.stringify(layergroup)
-        }, {}, function(res) {
-            assert.equal(res.statusCode, 400, res.body);
-            var parsed = JSON.parse(res.body);
-            assert.equal(parsed.errors.length, 1);
-            assert.ok(parsed.errors[0].match(/^style0/));
-            assert.ok(parsed.errors[0].match(/Unrecognized rule: backgxxxxxround-color/));
-            assert.ok(parsed.errors[0].match(/Unrecognized rule: foo/));
-            done();
-        });
+        assert.response(server,
+            {
+                url: layergroupUrl,
+                method: 'POST',
+                headers: {host: 'localhost', 'Content-Type': 'application/json' },
+                data: JSON.stringify(layergroup)
+            },
+            {
+                status: 400
+            },
+            function(res) {
+                var parsed = JSON.parse(res.body);
+                assert.equal(parsed.errors.length, 1);
+                assert.ok(parsed.errors[0].match(/^style0/));
+                assert.ok(parsed.errors[0].match(/Unrecognized rule: backgxxxxxround-color/));
+                assert.ok(parsed.errors[0].match(/Unrecognized rule: foo/));
+                done();
+            }
+        );
     });
 
 });
