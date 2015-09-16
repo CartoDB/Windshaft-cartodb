38e422e84c0d8aed2d694095d9c53b688f0f2aab
Raul Ochoa <rochoaf@gmail.com>
2015-09-16 21:54:56 +0200

    Moves sendError and sendResponse to Base controller
    Test for findStatusCode moved to controller

--- test/unit/cartodb/ported/windshaft_server.test.js
+++ test/unit/cartodb/ported/windshaft_server.test.js
@@ -41,20 +41,4 @@ describe('windshaft', function() {
         assert.equal(ws.base_url, '/tiles/:table');
     });
 
-    it('different formats for postgis plugin error returns 400 as status code', function() {
-
-        var expectedStatusCode = 400;
-        assert.equal(
-            cartodbServer.findStatusCode("Postgis Plugin: ERROR:  column \"missing\" does not exist\n"),
-            expectedStatusCode,
-            "Error status code for single line does not match"
-        );
-
-        assert.equal(
-            cartodbServer.findStatusCode("Postgis Plugin: PSQL error:\nERROR:  column \"missing\" does not exist\n"),
-            expectedStatusCode,
-            "Error status code for multiline/PSQL does not match"
-        );
-    });
-
 });
