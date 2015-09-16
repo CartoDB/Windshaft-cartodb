66f94d945286e4aa6f030f72bf86e3787b2b4af8
Raul Ochoa <rochoaf@gmail.com>
2015-09-16 02:49:18 +0200

    Fix test

--- test/unit/cartodb/ported/windshaft_server.test.js
+++ test/unit/cartodb/ported/windshaft_server.test.js
@@ -41,16 +41,16 @@ describe('windshaft', function() {
     });
 
     it('different formats for postgis plugin error returns 400 as status code', function() {
-        var ws = cartodbServer(serverOptions);
+
         var expectedStatusCode = 400;
         assert.equal(
-            ws.findStatusCode("Postgis Plugin: ERROR:  column \"missing\" does not exist\n"),
+            cartodbServer.findStatusCode("Postgis Plugin: ERROR:  column \"missing\" does not exist\n"),
             expectedStatusCode,
             "Error status code for single line does not match"
         );
 
         assert.equal(
-            ws.findStatusCode("Postgis Plugin: PSQL error:\nERROR:  column \"missing\" does not exist\n"),
+            cartodbServer.findStatusCode("Postgis Plugin: PSQL error:\nERROR:  column \"missing\" does not exist\n"),
             expectedStatusCode,
             "Error status code for multiline/PSQL does not match"
         );
