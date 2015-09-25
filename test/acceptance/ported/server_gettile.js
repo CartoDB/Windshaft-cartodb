62601e4252534692de4854b40f7ce92c0b84ae64
Raul Ochoa <rochoaf@gmail.com>
2015-09-25 16:06:19 +0200

    Remove redis keys after each test

--- test/acceptance/ported/server_gettile.js
+++ test/acceptance/ported/server_gettile.js
@@ -1,4 +1,4 @@
-require('../../support/test_helper');
+var testHelper = require('../../support/test_helper');
 
 var assert = require('../../support/assert');
 var fs = require('fs');
@@ -66,6 +66,10 @@ describe('server_gettile', function() {
 
         // Close the resources server
         res_serv.close(done);
+    });
+
+    afterEach(function(done) {
+        testHelper.deleteRedisKeys({'user:localhost:mapviews:global': 5}, done);
     });
 
     function imageCompareFn(fixture, done) {
