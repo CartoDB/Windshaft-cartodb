fc2f759dd32df474d79e7d83ddba4b74fe2ceecb
Raul Ochoa <rochoaf@gmail.com>
2015-09-25 14:39:39 +0200

    Remove redis keys after each test

--- test/acceptance/ported/blend_filtering.js
+++ test/acceptance/ported/blend_filtering.js
@@ -1,4 +1,4 @@
-require('../../support/test_helper');
+var testHelper = require('../../support/test_helper');
 
 var assert = require('../../support/assert');
 var testClient = require('./support/test_client');
@@ -34,6 +34,10 @@ describe('blend layer filtering', function() {
     after(function(done) {
         BaseController.prototype.req2params = req2paramsFn;
         httpRendererResourcesServer.close(done);
+    });
+
+    afterEach(function(done) {
+        testHelper.deleteRedisKeys({'user:localhost:mapviews:global': 5}, done);
     });
 
     var mapConfig = {
