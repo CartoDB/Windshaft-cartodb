1f6d5cfd6d85185d07dde8fc1e14e776a7219c62
Javier Goizueta <jgoizueta@gmail.com>
2016-01-21 18:39:31 +0100

    Fix signature of CDB_Overviews

--- test/support/sql/CDB_Overviews.sql
+++ test/support/sql/CDB_Overviews.sql
@@ -1,13 +1,13 @@
 -- Mockup for CDB_Overviews
-CREATE OR REPLACE FUNCTION CDB_Overviews(table_name text)
-RETURNS TABLE(z integer, overview_table text)
+CREATE OR REPLACE FUNCTION CDB_Overviews(table_name regclass)
+RETURNS TABLE(z integer, overview_table regclass)
 AS $$
   BEGIN
-    IF table_name = 'public.test_table_overviews' THEN
+    IF table_name::text = 'test_table_overviews' THEN
       RETURN QUERY
-        SELECT 1 AS z, 'test_table_overviews_ov1'::text AS overviw_table
+        SELECT 1 AS z, 'test_table_overviews_ov1'::regclass AS overviw_table
         UNION ALL
-        SELECT 2 AS z, 'test_table_overviews_ov2'::text AS overviw_table;
+        SELECT 2 AS z, 'test_table_overviews_ov2'::regclass AS overviw_table;
     ELSE
       RETURN;
     END IF;
