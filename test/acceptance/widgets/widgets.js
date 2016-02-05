74cb8767715124b11cb5c37013822c38ef0d60ac
Raul Ochoa <rochoaf@gmail.com>
2016-02-05 13:01:34 +0100

    Move list to own file

--- test/acceptance/widgets/widgets.js
+++ test/acceptance/widgets/widgets.js
@@ -3,50 +3,6 @@ var TestClient = require('../../support/test-client');
 
 describe('widgets', function() {
 
-    it("should expose layer list", function(done) {
-
-        var listWidgetMapConfig =  {
-            version: '1.5.0',
-            layers: [
-                {
-                    type: 'mapnik',
-                    options: {
-                        sql: 'select * from test_table',
-                        cartocss: '#layer { marker-fill: red; marker-width: 32; marker-allow-overlap: true; }',
-                        cartocss_version: '2.3.0',
-                        widgets: {
-                            names: {
-                                type: 'list',
-                                options: {
-                                    columns: ['name']
-                                }
-                            }
-                        }
-                    }
-                }
-            ]
-        };
-
-        var testClient = new TestClient(listWidgetMapConfig);
-
-        testClient.getWidget('names', function(err, res) {
-            if (err) {
-                return done(err);
-            }
-
-            var expectedList = [
-                {name:"Hawai"},
-                {name:"El Estocolmo"},
-                {name:"El Rey del Tallarín"},
-                {name:"El Lacón"},
-                {name:"El Pico"}
-            ];
-            assert.deepEqual(JSON.parse(res.body).rows, expectedList);
-
-            testClient.drain(done);
-        });
-    });
-
     it("should expose layer histogram", function(done) {
         var histogramMapConfig =  {
             version: '1.5.0',
