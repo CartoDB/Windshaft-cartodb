3979cda8c2ec1d57f581797e69c626c28a6e0902
Raul Ochoa <rochoaf@gmail.com>
2016-03-04 00:11:06 +0100

    Change comment about regex

--- test/support/sql/CDB_QueryStatements.sql
+++ test/support/sql/CDB_QueryStatements.sql
@@ -1,10 +1,10 @@
 -- DUMMY IMPLEMENTATION
 -- Ref: https://github.com/CartoDB/cartodb-postgresql/blob/master/scripts-available/CDB_QueryStatements.sql
+-- Originally implemented in plpython for performance reasons
 
 -- Return an array of statements found in the given query text
 --
 -- Regexp curtesy of Hubert Lubaczewski (depesz)
--- Implemented in plpython for performance reasons
 --
 CREATE OR REPLACE FUNCTION CDB_QueryStatements(query text)
 RETURNS SETOF TEXT AS $$
