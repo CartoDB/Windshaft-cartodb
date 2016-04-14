26149e7755825e204ee1d382bb9e0c0385a6dcb5
Raul Ochoa <rochoaf@gmail.com>
2016-04-14 11:53:48 +0200

    cdb_analysis_catalog is already retrieved from camshaft

--- test/support/sql/windshaft.test.sql
+++ test/support/sql/windshaft.test.sql
@@ -336,22 +336,6 @@ INSERT INTO _vovw_1_test_table_overviews VALUES
 
 -- analysis tables -----------------------------------------------
 
-
-
-drop table if exists cdb_analysis_catalog;
-create table cdb_analysis_catalog (
-    node_id char(40) CONSTRAINT cdb_analysis_catalog_pkey PRIMARY KEY,
-    analysis_def json NOT NULL,
-    input_nodes char(40) ARRAY NOT NULL DEFAULT '{}',
-    affected_tables regclass[] NOT NULL DEFAULT '{}',
-    cache_tables regclass[] NOT NULL DEFAULT '{}',
-    created_at timestamp with time zone NOT NULL DEFAULT now(),
-    updated_at timestamp with time zone NOT NULL DEFAULT now(),
-    used_at timestamp with time zone NOT NULL DEFAULT now(),
-    hits NUMERIC DEFAULT 0,
-    last_used_from char(40)
-);
-
 ALTER TABLE cdb_analysis_catalog OWNER TO :TESTUSER;
 
 
