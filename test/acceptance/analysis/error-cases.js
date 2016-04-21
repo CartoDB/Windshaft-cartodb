a11c8d882e4a4fbba688b1b2ed330f6d6438ab16
Raul Ochoa <rochoaf@gmail.com>
2016-04-21 17:27:20 +0200

    Each error-case will have different expectations

--- test/acceptance/analysis/error-cases.js
+++ test/acceptance/analysis/error-cases.js
@@ -4,7 +4,7 @@ var assert = require('../../support/assert');
 var TestClient = require('../../support/test-client');
 
 describe('analysis-layers error cases', function() {
-    function mapConfig(layers, dataviews, analysis) {
+    function createMapConfig(layers, dataviews, analysis) {
         return {
             version: '1.5.0',
             layers: layers,
@@ -13,45 +13,6 @@ describe('analysis-layers error cases', function() {
         };
     }
 
-    var useCases = [
-        {
-            desc: 'invalid source.id in layer',
-            fixture: 'buffer-over-source.png',
-            tile: { z: 7, x: 61, y: 47 },
-            mapConfig: mapConfig(
-                [
-                    {
-                        "type": "cartodb",
-                        "options": {
-                            "source": {
-                                "id": "INVALID-SOURCE-ID"
-                            },
-                            "cartocss": '#polygons { polygon-fill: red; }',
-                            "cartocss_version": "2.3.0"
-                        }
-                    }
-                ],
-                {},
-                [
-                    {
-                        "id": "HEAD",
-                        "type": "buffer",
-                        "params": {
-                            "source": {
-                                "id": "2570e105-7b37-40d2-bdf4-1af889598745",
-                                "type": "source",
-                                "params": {
-                                    "query": "select * from populated_places_simple_reduced"
-                                }
-                            },
-                            "radius": 50000
-                        }
-                    }
-                ]
-            )
-        }
-    ];
-
     var ERROR_RESPONSE = {
         status: 400,
         headers: {
@@ -59,19 +20,48 @@ describe('analysis-layers error cases', function() {
         }
     };
 
-    useCases.forEach(function(useCase) {
-        it('should implement use case: "' + useCase.desc + '"', function(done) {
+    it('should handle missing analysis nodes for layers', function(done) {
+        var mapConfig = createMapConfig(
+            [
+                {
+                    "type": "cartodb",
+                    "options": {
+                        "source": {
+                            "id": "INVALID-SOURCE-ID"
+                        },
+                        "cartocss": '#polygons { polygon-fill: red; }',
+                        "cartocss_version": "2.3.0"
+                    }
+                }
+            ],
+            {},
+            [
+                {
+                    "id": "HEAD",
+                    "type": "buffer",
+                    "params": {
+                        "source": {
+                            "id": "2570e105-7b37-40d2-bdf4-1af889598745",
+                            "type": "source",
+                            "params": {
+                                "query": "select * from populated_places_simple_reduced"
+                            }
+                        },
+                        "radius": 50000
+                    }
+                }
+            ]
+        );
 
-            var testClient = new TestClient(useCase.mapConfig, 1234);
+        var testClient = new TestClient(mapConfig, 1234);
 
-            testClient.getLayergroup(ERROR_RESPONSE, function(err, layergroupResult) {
-                assert.ok(!err, err);
+        testClient.getLayergroup(ERROR_RESPONSE, function(err, layergroupResult) {
+            assert.ok(!err, err);
 
-                assert.equal(layergroupResult.errors.length, 1);
-                assert.equal(layergroupResult.errors[0], 'Missing analysis node.id="INVALID-SOURCE-ID" for layer=0');
+            assert.equal(layergroupResult.errors.length, 1);
+            assert.equal(layergroupResult.errors[0], 'Missing analysis node.id="INVALID-SOURCE-ID" for layer=0');
 
-                testClient.drain(done);
-            });
+            testClient.drain(done);
         });
     });
 });
