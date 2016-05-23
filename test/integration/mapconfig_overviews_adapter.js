20725900b6e5d86ef900bc7a5bf996fed294f7a0
Raul Ochoa <rochoaf@gmail.com>
2016-05-23 13:22:46 +0200

    Overviews adapter into adapters package

--- test/integration/mapconfig_overviews_adapter.js
+++ test/integration/mapconfig_overviews_adapter.js
@@ -7,7 +7,7 @@ var PgConnection = require(__dirname + '/../../lib/cartodb/backends/pg_connectio
 var PgQueryRunner = require('../../lib/cartodb/backends/pg_query_runner');
 var OverviewsMetadataApi = require('../../lib/cartodb/api/overviews_metadata_api');
 var FilterStatsApi = require('../../lib/cartodb/api/filter_stats_api');
-var MapConfigOverviewsAdapter = require('../../lib/cartodb/models/mapconfig_overviews_adapter');
+var MapConfigOverviewsAdapter = require('../../lib/cartodb/models/mapconfig/adapter/mapconfig-overviews-adapter');
 
 // configure redis pool instance to use in tests
 var redisPool = new RedisPool(global.environment.redis);
