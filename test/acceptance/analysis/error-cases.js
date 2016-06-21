a2e74a3e1baba1799808ca372fe8d6f8e52aa2a4
Raul Ochoa <rochoaf@gmail.com>
2016-06-21 17:25:28 +0200

    Improve error messages for missing analyses for layers and dataviews

--- test/acceptance/analysis/error-cases.js
+++ test/acceptance/analysis/error-cases.js
@@ -72,6 +72,89 @@ describe('analysis-layers error cases', function() {
         });
     });
 
+    it('should handle missing analyses when layers point to nonexistent one', function(done) {
+        var mapConfig = createMapConfig(
+            [
+                {
+                    "type": "http",
+                    "options": {
+                        "urlTemplate": "http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png",
+                        "subdomains": "abcd"
+                    }
+                },
+                {
+                    "type": "cartodb",
+                    "options": {
+                        "source": {
+                            "id": "ID-FOR-NONEXISTENT-ANALYSIS"
+                        },
+                        "cartocss": '#polygons { polygon-fill: red; }',
+                        "cartocss_version": "2.3.0"
+                    }
+                }
+            ]
+        );
+
+        var testClient = new TestClient(mapConfig, 1234);
+
+        testClient.getLayergroup(ERROR_RESPONSE, function(err, layergroupResult) {
+            assert.ok(!err, err);
+
+            assert.equal(layergroupResult.errors.length, 1);
+            assert.equal(
+                layergroupResult.errors[0],
+                'Missing analysis node.id="ID-FOR-NONEXISTENT-ANALYSIS" for layer=1'
+            );
+
+            testClient.drain(done);
+        });
+    });
+
+    it('should handle missing analyses when dataviews point to nonexistent one', function(done) {
+        var mapConfig = createMapConfig(
+            [
+                {
+                    "type": "http",
+                    "options": {
+                        "urlTemplate": "http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png",
+                        "subdomains": "abcd"
+                    }
+                },
+                {
+                    "type": "cartodb",
+                    "options": {
+                        "sql": "select * from populated_places_simple_reduced",
+                        "cartocss": '#polygons { polygon-fill: red; }',
+                        "cartocss_version": "2.3.0"
+                    }
+                }
+            ],
+            {
+                pop_max_histogram: {
+                    source: {
+                        id: 'ID-FOR-NONEXISTENT-ANALYSIS'
+                    },
+                    type: 'histogram',
+                    options: {
+                        column: 'pop_max'
+                    }
+                }
+            }
+        );
+
+        var testClient = new TestClient(mapConfig, 1234);
+
+        testClient.getLayergroup(ERROR_RESPONSE, function(err, layergroupResult) {
+            assert.ok(!err, err);
+
+            assert.equal(layergroupResult.errors.length, 1);
+            assert.equal(layergroupResult.errors[0], 'Node with `source.id="ID-FOR-NONEXISTENT-ANALYSIS"`' +
+                ' not found in analyses for dataview "pop_max_histogram"');
+
+            testClient.drain(done);
+        });
+    });
+
     it('camshaft: should return error missing analysis nodes for layers with some context', function(done) {
         var mapConfig = createMapConfig(
             [
