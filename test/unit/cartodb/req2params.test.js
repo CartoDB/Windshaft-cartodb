3bab081438779d00e0f643cf3e219b133c3d3463
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-09-22 17:56:47 +0200

    Rename req2params by prepareContext

--- test/unit/cartodb/req2params.test.js
+++ test/unit/cartodb/req2params.test.js
@@ -7,17 +7,17 @@ var PgConnection = require('../../../lib/cartodb/backends/pg_connection');
 var AuthApi = require('../../../lib/cartodb/api/auth_api');
 var TemplateMaps = require('../../../lib/cartodb/backends/template_maps');
 
-var req2paramsMiddleware = require('../../../lib/cartodb/middleware/req2params-middleware');
+var prepareContextMiddleware = require('../../../lib/cartodb/middleware/prepare-context');
 var windshaft = require('windshaft');
 
-describe('req2params', function() {
+describe('prepare-context', function() {
 
     var test_user = _.template(global.environment.postgres_auth_user, {user_id:1});
     var test_pubuser = global.environment.postgres.user;
     var test_database = test_user + '_db';
 
 
-    var req2params;
+    var prepareContext;
     before(function() {
         var redisPool = new RedisPool(global.environment.redis);
         var mapStore = new windshaft.storage.MapStore();
@@ -26,12 +26,12 @@ describe('req2params', function() {
         var templateMaps = new TemplateMaps(redisPool);
         var authApi = new AuthApi(pgConnection, metadataBackend, mapStore, templateMaps);
 
-        req2params = req2paramsMiddleware(authApi, pgConnection);
+        prepareContext = prepareContextMiddleware(authApi, pgConnection);
     });
 
 
     it('can be found in server_options', function(){
-      assert.ok(_.isFunction(req2params));
+      assert.ok(_.isFunction(prepareContext));
     });
 
     function prepareRequest(req) {
@@ -45,7 +45,7 @@ describe('req2params', function() {
     it('cleans up request', function(done){
       var req = {headers: { host:'localhost' }, query: {dbuser:'hacker',dbname:'secret'}};
       var res = {};
-      req2params(prepareRequest(req), res, function(err, req) {
+      prepareContext(prepareRequest(req), res, function(err, req) {
           if ( err ) { done(err); return; }
           assert.ok(_.isObject(req.query), 'request has query');
           assert.ok(!req.query.hasOwnProperty('dbuser'), 'dbuser was removed from query');
@@ -60,7 +60,7 @@ describe('req2params', function() {
     it('sets dbname from redis metadata', function(done){
       var req = {headers: { host:'localhost' }, query: {} };
       var res = {};
-      req2params(prepareRequest(req), res, function(err, req) {
+      prepareContext(prepareRequest(req), res, function(err, req) {
           if ( err ) { done(err); return; }
           assert.ok(_.isObject(req.query), 'request has query');
           assert.ok(!req.query.hasOwnProperty('dbuser'), 'dbuser was removed from query');
@@ -75,7 +75,7 @@ describe('req2params', function() {
     it('sets also dbuser for authenticated requests', function(done){
       var req = {headers: { host:'localhost' }, query: {map_key: '1234'} };
       var res = {};
-      req2params(prepareRequest(req), res, function(err, req) {
+      prepareContext(prepareRequest(req), res, function(err, req) {
           if ( err ) { done(err); return; }
           assert.ok(_.isObject(req.query), 'request has query');
           assert.ok(!req.query.hasOwnProperty('dbuser'), 'dbuser was removed from query');
@@ -92,7 +92,7 @@ describe('req2params', function() {
                   map_key: '1235'
               }
           };
-          req2params(prepareRequest(req), res, function(err, req) {
+          prepareContext(prepareRequest(req), res, function(err, req) {
               // wrong key resets params to no user
               assert.ok(req.params.dbuser === test_pubuser, 'could inject dbuser ('+req.params.dbuser+')');
               done();
@@ -117,7 +117,7 @@ describe('req2params', function() {
         };
         var res = {};
 
-        req2params(prepareRequest(req), res, function(err, req) {
+        prepareContext(prepareRequest(req), res, function(err, req) {
             if ( err ) {
                 return done(err);
             }
