2989a276ce90649187c78ceae1e5cc9efbead935
Simon Martín <oleurud@gmail.com>
2017-11-10 13:14:28 +0100

    undo some small changes in tests

--- test/unit/cartodb/error_messages.test.js
+++ test/unit/cartodb/error_messages.test.js
@@ -11,8 +11,8 @@ describe('error messages clean up', function() {
         var inMessage = [
             "Postgis Plugin: Bad connection",
             "Connection string: 'host=127.0.0.1 port=5432 dbname=test_windshaft_cartodb_user_1_db " +
-            "user=test_windshaft_cartodb_user_1 connect_timeout=4'",
-            " encountered during parsing of layer 'layer0' in Layer"
+                "user=test_windshaft_cartodb_user_1 connect_timeout=4'",
+            "  encountered during parsing of layer 'layer0' in Layer"
         ].join('\n');
 
         const req = {};
