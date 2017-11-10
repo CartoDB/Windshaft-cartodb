c616d0831884742122ab5adee3260aca659359dd
Simon Martín <oleurud@gmail.com>
2017-11-10 13:00:02 +0100

    refactoring tests to new error middlewares

--- test/unit/cartodb/base_controller.js
+++ test/unit/cartodb/base_controller.js
@@ -1,23 +1,40 @@
 require('../../support/test_helper.js');
 
 var assert = require('assert');
-var errorMiddleware = require('../../../lib/cartodb/middleware/error-middleware');
+var statusCodeMiddleware = require('../../../lib/cartodb/middleware/error/status-code');
 
-describe('error-middleware', function() {
+describe('error-middleware: status code', function() {
 
-    it('different formats for postgis plugin error returns 400 as status code', function() {
+    const postgisErrors = [
+        {
+            error: "Postgis Plugin: ERROR:  column \"missing\" does not exist\n",
+            message: "Error status code for single line does not match"
+        },
+        {
+            error: "Postgis Plugin: PSQL error:\nERROR:  column \"missing\" does not exist\n",
+            message: "Error status code for multiline/PSQL does not match"
+        }
+    ];
 
-        var expectedStatusCode = 400;
-        assert.equal(
-            errorMiddleware.findStatusCode("Postgis Plugin: ERROR:  column \"missing\" does not exist\n"),
-            expectedStatusCode,
-            "Error status code for single line does not match"
-        );
-
-        assert.equal(
-            errorMiddleware.findStatusCode("Postgis Plugin: PSQL error:\nERROR:  column \"missing\" does not exist\n"),
-            expectedStatusCode,
-            "Error status code for multiline/PSQL does not match"
-        );
+    postgisErrors.forEach(postgisError => {
+        it('postgis plugin error ('+ postgisError.error +') returns 400 as status code', function() {
+            
+            const expectedStatusCode = 400;
+            const req = {};
+            let res = {
+                statusCode: null,
+                status(status) {
+                    this.statusCode = status;
+                }
+            };
+    
+            statusCodeMiddleware()(postgisError.error, req, res, function() {
+                assert.equal(
+                    res.statusCode,  
+                    expectedStatusCode,
+                    postgisError.message
+                );
+            });
+        });
     });
 });
--- test/unit/cartodb/error_messages.test.js
+++ test/unit/cartodb/error_messages.test.js
@@ -2,7 +2,7 @@ require('../../support/test_helper');
 
 var assert = require('assert');
 
-var errorMiddleware = require('../../../lib/cartodb/middleware/error-middleware');
+var errorResponseMiddleware = require('../../../lib/cartodb/middleware/error/error-response');
 
 describe('error messages clean up', function() {
 
@@ -11,12 +11,21 @@ describe('error messages clean up', function() {
         var inMessage = [
             "Postgis Plugin: Bad connection",
             "Connection string: 'host=127.0.0.1 port=5432 dbname=test_windshaft_cartodb_user_1_db " +
-                "user=test_windshaft_cartodb_user_1 connect_timeout=4'",
-            "  encountered during parsing of layer 'layer0' in Layer"
+            "user=test_windshaft_cartodb_user_1 connect_timeout=4'",
+            " encountered during parsing of layer 'layer0' in Layer"
         ].join('\n');
 
-        var outMessage = errorMiddleware.errorMessage(inMessage);
+        const req = {};
+        let res = {
+            errorResponseBody: null,
+            json(errorResponseBody) {
+                this.errorResponseBody = JSON.stringify(errorResponseBody);
+            }
+        };
 
+        errorResponseMiddleware()([inMessage], req, res);
+        var outMessage = res.errorResponseBody;
+    
         assert.ok(outMessage.match('connect'), outMessage);
         assert.ok(!outMessage.match(/666/), outMessage);
     });
