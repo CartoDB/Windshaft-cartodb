8d3503e0fce2d628a409cbd44497d50c0e9d8a89
Simon Martín <oleurud@gmail.com>
2018-04-27 15:46:57 +0200

    removing unneeded configuration

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -1,11 +1,9 @@
-export NPROCS=1 && export JOBS=1 && export CXX=g++-4.9 && export PGUSER=postgres
+# start PostgreSQL
+/etc/init.d/postgresql start
 
+# install dependencies
 npm install -g yarn@0.27.5
 yarn
 
- /etc/init.d/postgresql start
-
-createdb template_postgis && createuser publicuser
-psql -c "CREATE EXTENSION postgis" template_postgis
-
+# run tests
 POSTGIS_VERSION=2.4 npm test
