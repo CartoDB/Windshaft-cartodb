5407df03fad5e88425a82ebd52456d640a06a3a8
Javier Goizueta <jgoizueta@gmail.com>
2018-06-11 19:31:15 +0200

    Implement missing tests

--- test/acceptance/date-wrapping.spec.js
+++ test/acceptance/date-wrapping.spec.js
@@ -85,25 +85,181 @@ describe('date-wrapping', () => {
         });
     });
 
-
     describe('when a map instantiation has multiple layers', () => {
-        beforeEach(() => {
-            const mapConfig = mapConfigFactory.getVectorMapConfig({ numberOfLayers: 2 });
-            testClient = new TestClient(mapConfig);
-        });
+        afterEach(done => testClient.drain(done));
+
         describe('and both layers have the "dates_as_numbers" option enabled', () => {
-            // TODO: Pending test
-            it('should return dates as numbers for every layer');
+            beforeEach(() => {
+                const mapConfig = mapConfigFactory.getVectorMapConfig({
+                    numberOfLayers: 2,
+                    layerOptions: [
+                        { dates_as_numbers: true },
+                        { dates_as_numbers: true }
+                    ]
+                });
+                testClient = new TestClient(mapConfig);
+            });
+
+            it('should return dates as numbers for every layer', done => {
+                testClient.getLayergroup(function(err, layergroup) {
+                    assert.ifError(err);
+                    assert.deepEqual(layergroup.metadata.layers[0].meta.dates_as_numbers, ['date']);
+                    assert.deepEqual(layergroup.metadata.layers[1].meta.dates_as_numbers, ['date']);
+                });
+
+                testClient.getTile(0, 0, 0, { format: 'mvt' }, (err, res, mvt) => {
+                    const expected0 = [
+                        {
+                            type: 'Feature',
+                            id: 1,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 0, date: 1527810000 }
+                        },
+                        {
+                            type: 'Feature',
+                            id: 2,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 1, date: 1527900000 }
+                        }
+                    ];
+                    const expected1 = [
+                        {
+                            type: 'Feature',
+                            id: 1,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 0, date: 1527810000 }
+                        },
+                        {
+                            type: 'Feature',
+                            id: 2,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 1, date: 1527900000 }
+                        }
+                    ];
+                    const actual0 = JSON.parse(mvt.toGeoJSONSync(0)).features;
+                    const actual1 = JSON.parse(mvt.toGeoJSONSync(1)).features;
+
+                    assert.deepEqual(actual0, expected0);
+                    assert.deepEqual(actual1, expected1);
+                    done();
+                });
+            });
         });
 
         describe('and only one layers has the "dates_as_numbers" option enabled', () => {
-            // TODO: Pending test
-            it('should return dates as numbers only for the layer with the "dates_as_numbers" flag enabled');
+            beforeEach(() => {
+                const mapConfig = mapConfigFactory.getVectorMapConfig({
+                    numberOfLayers: 2,
+                    layerOptions: [
+                        { dates_as_numbers: false },
+                        { dates_as_numbers: true }
+                    ]
+                });
+                testClient = new TestClient(mapConfig);
+            });
+
+            it('should return dates as numbers only for the layer with the "dates_as_numbers" flag enabled',  done => {
+                testClient.getLayergroup(function(err, layergroup) {
+                    assert.ifError(err);
+                    assert.deepEqual(layergroup.metadata.layers[0].meta.dates_as_numbers || [], []);
+                    assert.deepEqual(layergroup.metadata.layers[1].meta.dates_as_numbers, ['date']);
+                });
+
+                testClient.getTile(0, 0, 0, { format: 'mvt' }, (err, res, mvt) => {
+                    const expected0 = [
+                        {
+                            type: 'Feature',
+                            id: 1,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 0 }
+                        },
+                        {
+                            type: 'Feature',
+                            id: 2,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 1 }
+                        }
+                    ];
+                    const expected1 = [
+                        {
+                            type: 'Feature',
+                            id: 1,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 0, date: 1527810000 }
+                        },
+                        {
+                            type: 'Feature',
+                            id: 2,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 1, date: 1527900000 }
+                        }
+                    ];
+                    const actual0 = JSON.parse(mvt.toGeoJSONSync(0)).features;
+                    const actual1 = JSON.parse(mvt.toGeoJSONSync(1)).features;
+
+                    assert.deepEqual(actual0, expected0);
+                    assert.deepEqual(actual1, expected1);
+                    done();
+                });
+            });
         });
 
         describe('and none of the layers has the "dates_as_numbers" option enabled', () => {
-            // TODO: Pending test
-            it('should return dates as dates for both layers');
+            beforeEach(() => {
+                const mapConfig = mapConfigFactory.getVectorMapConfig({
+                    numberOfLayers: 2,
+                    layerOptions: [
+                        { dates_as_numbers: false },
+                        { dates_as_numbers: false }
+                    ]
+                });
+                testClient = new TestClient(mapConfig);
+            });
+
+            it('should return dates as dates for both layers',  done => {
+                testClient.getLayergroup(function(err, layergroup) {
+                    assert.ifError(err);
+                    assert.deepEqual(layergroup.metadata.layers[0].meta.dates_as_numbers || [], []);
+                    assert.deepEqual(layergroup.metadata.layers[1].meta.dates_as_numbers || [], []);
+                });
+
+                testClient.getTile(0, 0, 0, { format: 'mvt' }, (err, res, mvt) => {
+                    const expected0 = [
+                        {
+                            type: 'Feature',
+                            id: 1,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 0 }
+                        },
+                        {
+                            type: 'Feature',
+                            id: 2,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 1 }
+                        }
+                    ];
+                    const expected1 = [
+                        {
+                            type: 'Feature',
+                            id: 1,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 0 }
+                        },
+                        {
+                            type: 'Feature',
+                            id: 2,
+                            geometry: { type: 'Point', coordinates: [0, 0] },
+                            properties: { _cdb_feature_count: 1, cartodb_id: 1 }
+                        }
+                    ];
+                    const actual0 = JSON.parse(mvt.toGeoJSONSync(0)).features;
+                    const actual1 = JSON.parse(mvt.toGeoJSONSync(1)).features;
+
+                    assert.deepEqual(actual0, expected0);
+                    assert.deepEqual(actual1, expected1);
+                    done();
+                });
+            });
         });
     });
 });
