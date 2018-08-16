31263b7b22698e9bfd48440ef57f3a88103db6ae
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-08-16 13:55:39 +0200

    Add unit test

---
+++ test/unit/cartodb/utils/date-wrapper.test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var dateWrapper = require('../../../../lib/cartodb/utils/date-wrapper');
+
+describe('date-wrapper', function() {
+
+    it('should wrap property fields with spaces', function() {
+        const actual = dateWrapper.wrapDates(
+          'select * from table',
+          [{name: 'a'}, {name: 'b c'}]
+        );
+        const expected = `
+        SELECT
+            "a","b c"
+        FROM
+            (select * from table) _cdb_epoch_transformation `;
+        assert.equal(actual, expected);
+    });
+});
