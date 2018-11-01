3bb1f893af9bb37b3c84583ec725f4152fa1dbf0
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-11-01 15:05:38 +0100

    Test only

--- test/acceptance/user-database-timeout-limit.js
+++ test/acceptance/user-database-timeout-limit.js
@@ -432,7 +432,7 @@ describe('user database timeout limit', function () {
                     this.testClient.setUserDatabaseTimeoutLimit(0, done);
                 });
 
-                it('"mvt" fails due to statement timeout', function (done) {
+                it.only('"mvt" fails due to statement timeout', function (done) {
                     const params = {
                         layergroupid: this.layergroupid,
                         format: 'mvt',
