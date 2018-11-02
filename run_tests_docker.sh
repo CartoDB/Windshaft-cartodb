f8fa78bb8bb410a212892d835e7ac3ee30407ffc
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-11-02 13:30:24 +0100

    Output npm version

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -19,7 +19,7 @@ NODEJS_VERSION=${1-nodejs10}
 
 if [ "$NODEJS_VERSION" = "nodejs10" ];
 then
-    echo "Node.js version:"
+    echo "npm version on install:"
     npm -v
     npm install
 elif [ "$NODEJS_VERSION" = "nodejs6" ];
@@ -31,4 +31,7 @@ else
 fi
 
 # run tests
+echo "npm version on tests:"
+npm -v
+
 npm test
