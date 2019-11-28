8f99886d62416d84b64beedb1a243984e396d5a7
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-11-28 19:46:22 +0100

    Clean script

--- docker/scripts/test-setup.sh
+++ docker/scripts/test-setup.sh
@@ -8,15 +8,5 @@ source /src/nodejs-install.sh
 git clone https://github.com/CartoDB/cartodb-postgresql.git
 cd cartodb-postgresql && make && make install && cd ..
 
-echo "Node.js version: "
-node -v
-
-echo "npm version: "
-npm -v
-
-echo "Clean install: "
 npm ci
-npm ls
-
-# run tests
 npm test
