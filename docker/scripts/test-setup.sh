7081a7ec3c6418dfcc10036aa2624f9ade6d7330
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-12-01 13:04:56 +0100

    Better script organization

--- docker/scripts/test-setup.sh
+++ docker/scripts/test-setup.sh
@@ -8,5 +8,7 @@ source /src/nodejs-install.sh
 git clone https://github.com/CartoDB/cartodb-postgresql.git
 cd cartodb-postgresql && make && make install && cd ..
 
+cp config/environments/test.js.example config/environments/test.js
+
 npm ci
 npm test
