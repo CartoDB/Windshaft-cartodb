65172202ec76c0c4900e7524a3df778fddc4f9a0
alberhander <albertoh@carto.com>
2021-01-14 09:57:06 +0100

    add missing space

--- config/environments/config.js
+++ config/environments/config.js
@@ -65,7 +65,7 @@ var config = {
     //  3. {{=it.port}}: will use the `port` from this very same configuration file.
     resources_url_templates: {
         http: process.env.CARTO_WINDSHAFT_RESOURCE_URL_TEMPLATE_HTTP || 'http://{{=it.cdn_url}}/{{=it.user}}/api/v1/map',
-        https: process.env.CARTO_WINDSHAFT_RESOURCE_URL_TEMPLATE_HTTPS ||'https://{{=it.cdn_url}}/{{=it.user}}/api/v1/map'
+        https: process.env.CARTO_WINDSHAFT_RESOURCE_URL_TEMPLATE_HTTPS || 'https://{{=it.cdn_url}}/{{=it.user}}/api/v1/map'
     },
     // Specify the maximum length of the queue of pending connections for the HTTP server.
     // The actual length will be determined by the OS through sysctl settings such as tcp_max_syn_backlog and somaxconn on Linux.
